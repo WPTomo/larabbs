3f2956235ae93e9f9f94c364ce15e1bd9109a725
wptomo <wptomo1106@gmail.com>
2018-05-15 15:47:58 +0800

    notifications read

--- routes/api.php
+++ routes/api.php
@@ -95,6 +95,12 @@ $api->version('v1', [
             // 通知列表
             $api->get('user/notifications', 'NotificationsController@index')
                 ->name('api.user.notifications.index');
+            // 未读消息统计
+            $api->get('user/notifications/stats', 'NotificationsController@stats')
+                ->name('api.user.notifications.stats');
+            // 标记消息通知为已读
+            $api->patch('user/read/notifications', 'NotificationsController@read')
+                ->name('api.user.notifications.read');
         });
     });
 });
