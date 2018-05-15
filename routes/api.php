1b2b703d8c802d648f0df1fcda284653a5f78845
wptomo <wptomo1106@gmail.com>
2018-05-15 14:37:12 +0800

    replies destroy

--- routes/api.php
+++ routes/api.php
@@ -80,6 +80,12 @@ $api->version('v1', [
             // 删除话题
             $api->delete('topics/{topic}', 'TopicsController@destroy')
                 ->name('api.topics.destroy');
+            // 发表回复
+            $api->post('topics/{topic}/replies', 'RepliesController@store')
+                ->name('api.topics.replies.store');
+            // 删除回复
+            $api->delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')
+                ->name('api.topics.replies.destroy');
         });
     });
 });
